453d125cee38a1ca33d692dec60ce8917f6a63dc
ashish-doing <ashish.kumar.doing@gmail.com>
2026-06-14 13:19:01 +0530

    disable SSR - fixes blank gr.HTML rendering with custom JS bridge

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 from ui_photo import demo
 import tempfile
-demo.queue().launch(show_api=False, allowed_paths=[tempfile.gettempdir()])
+
+demo.queue().launch(
+    show_api=False,
+    allowed_paths=[tempfile.gettempdir()],
+    ssr_mode=False,
+)
